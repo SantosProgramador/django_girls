4135cbb644b3ff968adb80c9e2c77624dd3ce8b9
root <root@ayoub-VirtualBox.(none)>
2016-04-18 10:21:47 +0200

    Commit atomico 2

--- blog/views.py
+++ blog/views.py
@@ -1,67 +1,123 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+
 from django.utils import timezone
+
 from .models import Post, Comment
+
 from .forms import PostForm, CommentForm
-from django.shortcuts import redirect
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
 
+from django.views.generic import ListView,DetailView,CreateView,DeleteView, UpdateView
+
+from django.core.urlresolvers import reverse
+
 # Create your views here.
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#def post_list(request):
+    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # punto de ruptura -> import ipdb; ipdb.set_trace()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    #return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk) 
-    comments = Comment.objects.filter(post=post).order_by('created_date')
+class PostList(ListView):
+    context_object_name = 'posts'
     
-    if request.method == "POST":
-        form = CommentForm(request.POST)
 
-        if form.is_valid():
-            if request.user.is_authenticated():
-                comment = form.save(commit=False)
-                comment.author = request.user
-                comment.post = post
-                comment.save()
-                return redirect('blog.views.post_detail', pk=post.pk)
-            else:
-                return redirect('login')
-    else:
-        form = CommentForm()
+    def get_queryset(self):    
+        queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+        return queryset
 
-    return render(request, 'blog/post_detail.html', {'post': post, 'comments':comments, 'form': form})
+    # Ampliar el contexto 
+    #def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        #context = super(PostList, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        #context['posts'] = Post.objects.all()
+        #return context
 
-@login_required(login_url='login')
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
+#def post_detail(request, pk):
+    #post = get_object_or_404(Post, pk=pk) 
+    #comments = Comment.objects.filter(post=post).order_by('created_date')
+    
+    #if request.method == "POST":
+        #form = CommentForm(request.POST)
 
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('blog.views.post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+        #if form.is_valid():
+            #if request.user.is_authenticated():
+                #comment = form.save(commit=False)
+                #comment.author = request.user
+                #comment.post = post
+                #comment.save()
+                #return redirect('blog.views.post_detail', pk=post.pk)
+            #else:
+                #return redirect('login')
+    #else:
+        #form = CommentForm()
 
-@login_required(login_url='login')
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('blog.views.post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form, 'post': post})
+    #return render(request, 'blog/post_detail.html', {'post': post, 'comments':comments, 'form': form})
+
+
+class PostDetail(DetailView):
+    model = Post
+
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title','text']
+    template_name = 'blog/post_edit.html'
+    #success_url = reverse('post_detail')
+    
+    def get_success_url(self):
+        return reverse('post_detail', kwargs={'pk':self.object.pk})
+
+    def form_valid(self, form):
+        form.post = form.instance
+        form.post.author = self.request.user
+        form.post.published_date = timezone.now()
+        return super(PostCreate,self).form_valid(form)
+
+    
+    
+#@login_required(login_url='login')
+# def post_new(request):
+#     if request.method == "POST":
+#         form = PostForm(request.POST)
+
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.published_date = timezone.now()
+#             post.save()
+#             return redirect('blog.views.post_detail', pk=post.pk)
+#     else:
+#         form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
+
+class EditPost(UpdateView):
+    model = Post
+    fields = ['title','text']
+    template_name = 'blog/post_edit.html'
+
+    def get_success_url(self):
+        return reverse('post_detail', kwargs={'pk':self.object.pk})
+# @login_required(login_url='login')
+# def post_edit(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     if request.method == "POST":
+#         form = PostForm(request.POST, instance=post)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.save()
+#             return redirect('blog.views.post_detail', pk=post.pk)
+#     else:
+#         form = PostForm(instance=post)
+#     return render(request, 'blog/post_edit.html', {'form': form, 'post': post})
+
+# class PostDelete(DeleteView):
+#     model = Post
+
 
 @login_required(login_url='login')
 def post_delete(request,pk):
